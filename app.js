6e8542f58424f920604a04adf537e9905a178791
giuseppeesposito22 <gius.espo22@gmail.com>
2025-05-16 15:49:04 +0200

    import middleware in app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Importazione dei moduli
 const { url, port } = require("./data/db");
 const postRouters = require("./routers/posts");
+const errorHandler = require("./middlewares/errorHandler");
+const notFound = require("./middlewares/notFound");
 
 // Configuaazione app js
 const express = require("express");
@@ -13,6 +15,10 @@ app.use(express.json());
 
 // Utilizzo delle routers con architetttura REST
 app.use("/posts", postRouters);
+
+// Middlewares
+app.use(errorHandler);
+app.use(notFound);
 
 app.listen(port, () => {
   console.log(`Collegato alla porta ${url}:${port}`);
